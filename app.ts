98e250ae294c8160e414fdc6d4a1647cdb46ec93
dldnjswns31 <dldnjswns32@gmail.com>
2023-02-03 19:20:31 +0900

    refactor: 타입 지정

--- app.ts
+++ app.ts
@@ -23,20 +23,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req: Request, res: Response, next: NextFunction) {
+app.use((req: Request, res: Response, next: NextFunction) => {
   next(createError(404));
 });
 
-// error handler
-
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
 };
 app.use(errorHandler);
