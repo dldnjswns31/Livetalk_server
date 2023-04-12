72d363b46513246c916c9f3a35d653c2e8d77784
dldnjswns31 <dldnjswns32@gmail.com>
2023-04-13 00:24:02 +0900

    chores: vercel 배포 관련 패키지 삭제 및 script 수정

--- dist/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("./../controllers/authController");
-const authRouter = (0, express_1.Router)();
-authRouter.post("/signup", authController_1.signup);
-authRouter.post("/signin", authController_1.signin);
-authRouter.get("/verify", authController_1.verify);
-exports.default = authRouter;
--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-let indexRouter = (0, express_1.Router)();
-indexRouter.get("/", function (req, res, next) {
-    console.log("index");
-    res.send("hello");
-});
-exports.default = indexRouter;
